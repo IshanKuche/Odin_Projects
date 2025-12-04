f8cc122f07d7fd9204416546df6d0852e14065ee
ishan <ishankuche@gmail.com>
2025-12-04 05:35:24 +0000

    Fix nested repo issue and organize folders

---
+++ jspractice/index.js
@@ -0,0 +1,6 @@
+// Single line comment
+/* multiline comment */
+console.log("Hello World") // print output
+window.alert("this is an alert"); // pop-up notification
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `this is demo`
---
+++ jspractice/practice/function.js
@@ -0,0 +1,7 @@
+function pow(x,n){
+    return(x**n)
+}
+
+let a = prompt("enter number 1: ")
+let b = prompt("enter number 2: ")
+console.log(pow(a,b))
